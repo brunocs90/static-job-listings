de780e3cc18ee25b7cf13ce874af39ed454c7eb9
brunocs90@gmail.com <brunocs90@gmail.com>
2023-03-06 16:44:45 -0300

    css default

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,16 +6,18 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 }
-:focus {
-  outline: 0;
-  box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']}
-}
 body {
-  background: ${({ theme }) => theme['gray-800']};
-  color: ${({ theme }) => theme['gray-100']};
-  -webkit-font-smoothing: antialiased;
+    background: ${({ theme }) => theme.colors.background};
+    -webkit-font-smoothing: antialiased;
 }
+
 body, input, textarea, button {
-  font: 400 1rem Roboto, sans-serif;
+    font-family: ${({ theme }) => theme['font-family']};
+    font-size: ${({ theme }) => theme.Typography.body};
 }
+
+a {
+  text-decoration: none;
+}
+
 `;
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,20 +1,20 @@
 export const defaultTheme = {
-    white: '#fff',
-
-    'gray-100': '#E1E1E6',
-    'gray-300': '#C4C4CC',
-    'gray-400': '#8D8D99',
-    'gray-500': '#7C7C8A',
-    'gray-600': '#323238',
-    'gray-700': '#29292E',
-    'gray-800': '#202024',
-    'gray-900': '#121214',
-
-    'green-300': '#00B37E',
-    'green-500': '#00875F',
-    'green-700': '#015F43',
-
-    'red-300': '#F75A68',
-    'red-500': '#AB222E',
-    'red-700': '#7A1921',
+    colors: {
+        'desaturated-dark': '#5CA5A5',
+        background: '#EFFAFA',
+        'dark-grayish': '#7C8F8F',
+        'very-dark-grayish': '#2B3939',
+        white: '#FFFFFF',
+    },
+    'font-family': "'League Spartan', 'sans-serif'",
+    Typography: {
+        body: {
+            'font-size': '15px',
+            'font-weight': '500',
+        },
+        heading: {
+            'font-size': '15px',
+            'font-weight': '700',
+        },
+    },
 } as const;
